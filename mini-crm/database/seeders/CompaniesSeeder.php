5e4ff4b6ceffb2e3c08567b1762f5ded6adf1dea
afdha12 <azri.afdha@icloud.com>
2022-10-10 14:21:03 +0700

    update to CompaniesSeeder

--- mini-crm/database/seeders/CompaniesSeeder.php
+++ mini-crm/database/seeders/CompaniesSeeder.php
@@ -16,10 +16,28 @@ class CompaniesSeeder extends Seeder
     public function run()
     {
         Companies::insert([
-            'name'=>'Google',
-            'email'=>'press@google.com',
-            'logo'=>'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/272px-Google_2015_logo.svg.png',
-            'website'=>'https://about.google/'
+            'name'=>'Shopee',
+            'email'=>'help@support.shopee.co.id',
+            'logo'=>'https://upload.wikimedia.org/wikipedia/commons/f/fe/Shopee.svg',
+            'website'=>'https://shopee.co.id/'
+        ]);
+        Companies::insert([
+            'name'=>'Tokopedia',
+            'email'=>'pr@tokopedia.com',
+            'logo'=>'https://jarisnk.com/wp-content/uploads/2019/10/Logo-Tokopedia.png',
+            'website'=>'https://www.tokopedia.com/'
+        ]);
+        Companies::insert([
+            'name'=>'Telkom Indonesia',
+            'email'=>'corporate_comm@telkom.co.id',
+            'logo'=>'https://www.telkom.co.id/data/image_upload/page/1594112773573_compress_PNG%20Logo%20Sekunder%20Telkom.png',
+            'website'=>'corporate_comm@telkom.co.id'
+        ]);
+        Companies::insert([
+            'name'=>'Pertamina',
+            'email'=>'pcc135@pertamina.com',
+            'logo'=>'https://www.pertamina.com/landing/images/logo.png',
+            'website'=>'https://www.pertamina.com/'
         ]);
     }
 }
