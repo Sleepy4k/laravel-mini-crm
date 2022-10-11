b610922f99b93a4fa52eeb0b1ac2be292be721bf
afdha12 <azri.afdha@icloud.com>
2022-10-11 12:16:57 +0700

    add method for CRUD

--- mini-crm/app/Http/Controllers/CompaniesController.php
+++ mini-crm/app/Http/Controllers/CompaniesController.php
@@ -9,6 +9,32 @@ class CompaniesController extends Controller
 {
     public function index(){
         $class = Companies::all();
-        return view('companies', ['companies'=>$class]);
+        return view('companies.companies', ['companies'=>$class]);
+    }
+
+    public function add(){
+        return view('companies.add');
+    }
+
+    public function save(Request $request){
+        Companies::create($request->all());
+        return redirect('/companies');
+    }
+
+    public function getdata($id){
+        $data = Companies::find($id);
+        return view('companies.editform', compact('data'));
+        //dd($data);
+    }
+    public function edit(Request $request, $id){
+        $data = Companies::find($id);
+        $data->update($request->all());
+        return redirect('/companies')->with('success','Data Berhasil Diupdate');
+    }
+
+    public function delete($id){
+        $data = Companies::find($id);
+        $data->delete();
+        return redirect('/companies')->with('success','Data Berhasil Dihapus');
     }
 }
--- mini-crm/app/Http/Controllers/EmployeesController.php
+++ mini-crm/app/Http/Controllers/EmployeesController.php
@@ -9,6 +9,32 @@ class EmployeesController extends Controller
 {
     public function index(){
         $class = Employees::all();
-        return view('employees', ['employees'=>$class]);
+        return view('employees/employees', ['employees'=>$class]);
+    }
+    
+    public function add(){
+        return view('employees.add');
+    }
+
+    public function save(Request $request){
+        Employees::create($request->all());
+        return redirect('/employees');
+    }
+
+    public function getdata($id){
+        $data = Employees::find($id);
+        return view('employees.editform', compact('data'));
+        //dd($data);
+    }
+    public function edit(Request $request, $id){
+        $data = Employees::find($id);
+        $data->update($request->all());
+        return redirect('/employees')->with('success','Data Berhasil Diupdate');
+    }
+
+    public function delete($id){
+        $data = Employees::find($id);
+        $data->delete();
+        return redirect('/employees')->with('success','Data Berhasil Dihapus');
     }
 }
