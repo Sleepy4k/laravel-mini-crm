5d238851496e75af134bee1ef6948b7f6872bc12
afdha12 <azri.afdha@icloud.com>
2022-10-11 12:17:33 +0700

    update routes

--- mini-crm/routes/web.php
+++ mini-crm/routes/web.php
@@ -22,5 +22,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'login'])->name('login');
 Route::post('/', [UserController::class, 'authenticate'])->name('authenticate');
+
+// Companies Routes
 Route::get('/companies', [CompaniesController::class, 'index'])->name('index');
+Route::get('/companies_add', [CompaniesController::class, 'add'])->name('add');
+Route::post('/companies_add', [CompaniesController::class, 'save'])->name('save');
+Route::get('/getcompanies/{id}', [CompaniesController::class, 'getdata'])->name('getdata');
+Route::post('/updatecompanies/{id}', [CompaniesController::class, 'edit'])->name('edit');
+Route::get('/deletecompanies/{id}', [CompaniesController::class, 'delete'])->name('delete');
+
+// Employees Routes
 Route::get('/employees', [EmployeesController::class, 'index'])->name('index');
+Route::get('/employees_add', [EmployeesController::class, 'add'])->name('add');
+Route::post('/employees_add', [EmployeesController::class, 'save'])->name('save');
+Route::get('/getemployees/{id}', [EmployeesController::class, 'getdata'])->name('getdata');
+Route::post('/updateemployees/{id}', [EmployeesController::class, 'edit'])->name('edit');
+Route::get('/deleteemployees/{id}', [EmployeesController::class, 'delete'])->name('delete');
